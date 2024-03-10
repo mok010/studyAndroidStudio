bd511f0776431dc3c0233e39cf3038e6679cee10
violet <blueangle@gmail.com>
2024-03-10 22:07:53 +0900

    clickButton

--- app/src/main/java/org/techtown/test/MainActivity.java
+++ app/src/main/java/org/techtown/test/MainActivity.java
@@ -1,6 +1,8 @@
 package org.techtown.test;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    ImageView imageView2;
+    int imageIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onButton1Clicked(View v) {
+        changeImage();
+    }
+
+    public void changeImage() {
+        if (imageIndex == 0) {
+            imageView.setVisibility(imageView.VISIBLE);
+            imageView2.setVisibility(imageView.INVISIBLE);
+
+            imageIndex = 1;
+        }
+        else if (imageIndex == 1) {
+            imageView2.setVisibility(imageView.VISIBLE);
+            imageView.setVisibility(imageView.INVISIBLE);
+
+            imageIndex = 0;
+        }
     }
 }
