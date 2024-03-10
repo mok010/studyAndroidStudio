1a90b41515e286dc8f3b5cbd7d9bc6e36e219e44
violet <blueangle@gmail.com>
2024-03-10 22:12:50 +0900

    scroll

--- app/src/main/java/org/techtown/test/MainActivity.java
+++ app/src/main/java/org/techtown/test/MainActivity.java
@@ -11,40 +11,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;
-    ImageView imageView2;
-    int imageIndex = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageView);
-        imageView2 = findViewById(R.id.imageView2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void onButton1Clicked(View v) {
-        changeImage();
-    }
-
-    public void changeImage() {
-        if (imageIndex == 0) {
-            imageView.setVisibility(imageView.VISIBLE);
-            imageView2.setVisibility(imageView.INVISIBLE);
-
-            imageIndex = 1;
-        }
-        else if (imageIndex == 1) {
-            imageView2.setVisibility(imageView.VISIBLE);
-            imageView.setVisibility(imageView.INVISIBLE);
-
-            imageIndex = 0;
-        }
     }
 }
