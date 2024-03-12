c720f84d4d1409ca07f139c89695b3d19bc43358
violet <blueangle1004@gmail.com>
2024-03-12 22:03:50 +0900

    direction

--- app/src/main/java/org/techtown/test/MainActivity.java
+++ app/src/main/java/org/techtown/test/MainActivity.java
@@ -1,90 +1,36 @@
 package org.techtown.test;
 
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    String name;
+    EditText editText;
 
-    GestureDetector detector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
-        View view = findViewById(R.id.view);
-        view.setOnTouchListener(new View.OnTouchListener() {
+
+        showToast("onCreate 호출됨");
+
+        editText = findViewById(R.id.editText);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                int action = motionEvent.getAction();
-                float curX = motionEvent.getX();
-                float curY = motionEvent.getY();
+            public void onClick(View view) {
 
-                if (action == MotionEvent.ACTION_DOWN) {
-                    println("손가락 눌림 : " + curX + ", " + curY);
-                } else if (action == MotionEvent.ACTION_MOVE) {
-                    println("손가락 움직임 : " + curX + ", " + curY);
-                } else if (action == MotionEvent.ACTION_UP) {
-                    println("손가락 뗌 : " + curX + ", " + curY);
-                }
-
-                return true;
-            }
-        });
-
-        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
-            @Override
-            public boolean onDown(@NonNull MotionEvent motionEvent) {
-                println("onDown 호출");
-                return true;
-            }
-
-            @Override
-            public void onShowPress(@NonNull MotionEvent motionEvent) {
-
-            }
-
-            @Override
-            public boolean onSingleTapUp(@NonNull MotionEvent motionEvent) {
-                return false;
-            }
-
-            @Override
-            public boolean onScroll(@Nullable MotionEvent motionEvent, @NonNull MotionEvent motionEvent1, float v, float v1) {
-                return false;
-            }
-
-            @Override
-            public void onLongPress(@NonNull MotionEvent motionEvent) {
-                println("onLongPress 호출");
-            }
-
-            @Override
-            public boolean onFling(@Nullable MotionEvent motionEvent, @NonNull MotionEvent motionEvent1, float v, float v1) {
-                println("onFling 호출 : " + v + ", " + v1);
-                return true;
-            }
-        });
-
-        View view2 = findViewById(R.id.view2);
-        view2.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                detector.onTouchEvent(event);
-                return true;
             }
         });
 
@@ -95,7 +41,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void println(String data) {
-        textView.append((data + "\n"));
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        showToast("onDestroy 호출됨");
     }
+
+    public void showToast(String data) {
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+    }
+
 }
