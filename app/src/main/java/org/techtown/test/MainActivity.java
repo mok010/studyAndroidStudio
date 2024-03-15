1ad8903c50931a12a96a31bb0009ca37eab2cf94
violet <blueangle1004@gmail.com>
2024-03-15 16:03:20 +0900

    rotation with big change

--- app/src/main/java/org/techtown/test/MainActivity.java
+++ app/src/main/java/org/techtown/test/MainActivity.java
@@ -4,17 +4,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     String name;
     EditText editText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +30,44 @@ public class MainActivity extends AppCompatActivity {
         showToast("onCreate 호출됨");
 
         editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
 
         Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (editText != null) {
+                        name = editText.getText().toString();
+                        showToast("사용자 입력값을 name변수에 할당함");
+                    }
+                }
+            });
 
+        }
+
+        if (savedInstanceState != null) {
+            if (textView != null) {
+                name = savedInstanceState.getString("name");
+                textView.setText(name);
+
+                showToast("값을 복원했습니다 : " + name);
             }
-        });
+        }
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("name", name);
     }
 
     @Override
